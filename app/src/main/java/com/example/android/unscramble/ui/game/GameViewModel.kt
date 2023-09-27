48586d32addc72a80460397efee58b31b514ff36
Senoes <senosatrio46@gmail.com>
2023-09-27 21:34:56 +0700

    Mengisi View Model Di GameViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -7,7 +7,7 @@ class GameViewModel : ViewModel() {
 
     var score = 0
     private var currentWordCount = 0
-    private var currentScrambledWord = "testt"
+    private lateinit var currentScrambledWord: String
 
     val currentScrambleWord: String
         get() = currentScrambleWord
@@ -15,9 +15,38 @@ class GameViewModel : ViewModel() {
 
     init {
         Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
     }
     override fun onCleared(){
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
+
+        fun nextWord(): Boolean {
+            return if (currentWordCount < MAX_NO_OF_WORDS) {
+                getNextWord()
+                true
+            } else false
+        }
     }
 }
