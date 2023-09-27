e308aa636c7b3901df67e356fd2ca659a3560d4c
Amigo Wahyu <amigo2018wahyu@gmail.com>
2023-09-27 14:13:05 +0700

    Menambahkan data ke ViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,5 +3,11 @@ package com.example.android.unscramble.ui.game
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    private val viewModel = GameViewModel()
+
+    var score = 0
+    private var currentWordCount = 0
+    private var currentScrambledWord = "test"
+
+    val currentScrambleWord: String
+        get() = currentScrambleWord
 }
